f1d852f192ebbc3e0a6150a0fcaa71405c787812
José Lambertus <jlambertus@ucsd.edu.do>
2026-07-23 07:37:20 -0400

    feat: install Laravel Fortify authentication backend

--- apps/web/bootstrap/providers.php
+++ apps/web/bootstrap/providers.php
@@ -1,11 +1,7 @@
 <?php
 
-declare(strict_types=1);
-
-use App\Providers\AppServiceProvider;
-use App\Providers\HorizonServiceProvider;
-
 return [
-    AppServiceProvider::class,
-    HorizonServiceProvider::class,
+    App\Providers\AppServiceProvider::class,
+    App\Providers\FortifyServiceProvider::class,
+    App\Providers\HorizonServiceProvider::class,
 ];
